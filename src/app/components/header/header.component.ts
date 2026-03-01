20235687c6bda412c70785bd4dae3296c21d75a1
Ivo Rojas <ivo753@gmail.com>
2026-03-01 18:15:43 -0400

    implemented PWA connection status.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ProductService } from '../../services/product.service';
 import { CartService } from '../../services/cart.service';
 import { VisitorService } from '../../services/visitor.service';
+import { PwaService } from '../../services/pwa.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule, MatSnackBarModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -18,6 +21,9 @@ export class HeaderComponent {
   private readonly productService = inject(ProductService);
   private readonly cartService = inject(CartService);
   private readonly visitorService = inject(VisitorService);
+  private readonly pwaService = inject(PwaService);
+
+  readonly isOnline = this.pwaService.isOnline;
 
   /**
    * Resets the entire application state
