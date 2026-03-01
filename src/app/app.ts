16177ef467069fe292cd45a15a47ada9ea3efacf
Ivo Rojas <ivo753@gmail.com>
2026-03-01 15:31:36 -0400

    Implemented product list with cdk virtual scroll and dynamic loading.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, WelcomeComponent],
+  imports: [HeaderComponent, WelcomeComponent, ProductListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
